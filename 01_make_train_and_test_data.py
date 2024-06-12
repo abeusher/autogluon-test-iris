150895201bde38918d0b01a2422d59a43a6c48e9
Abe Usher <abe.usher@gmail.com>
2024-06-12 09:19:57 -0400

    update

--- 01_make_train_and_test_data.py
+++ 01_make_train_and_test_data.py
@@ -1,5 +1,11 @@
 from sklearn.model_selection import train_test_split
 
+"""
+Simple script to split the IRIS dataset into training and testing sets.
+
+I obtained the data file from: https://www.kaggle.com/datasets/arshid/iris-flower-dataset
+"""
+
 # Split the data into training and testing sets
 train_data, test_data = train_test_split(iris_data, test_size=0.2, random_state=42)
 
---
+++ 03_advanced_autogluon.py
@@ -0,0 +1,18 @@
+from autogluon.tabular import TabularDataset, TabularPredictor
+
+
+# load two data sets:
+#  - "train_data" for training 
+#  -  "test_data" for testing
+train_data = TabularDataset('data/iris_train.csv')
+test_data = TabularDataset('data/iris_test.csv')
+
+# create a predictor object and fit it to the training data
+predictor = TabularPredictor(label='species').fit(train_data=train_data)
+
+# make predictions on the test set
+#predictions = predictor.predict(test_data)
+
+# evaluate the model
+leaderboard = predictor.leaderboard(test_data)
+print(leaderboard)
