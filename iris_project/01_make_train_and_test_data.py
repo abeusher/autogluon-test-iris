b5c1ca0b0a96f1ca4749fc56d364709bf4d15fa1
Abe Usher <abe.usher@gmail.com>
2024-06-12 11:50:47 -0400

    update

--- iris_project/01_make_train_and_test_data.py
+++ iris_project/01_make_train_and_test_data.py
@@ -10,8 +10,8 @@ I obtained the data file from: https://www.kaggle.com/datasets/arshid/iris-flowe
 train_data, test_data = train_test_split(iris_data, test_size=0.2, random_state=42)
 
 # Save the split data into separate CSV files
-train_data_path = '/mnt/data/IRIS_train.csv'
-test_data_path = '/mnt/data/IRIS_test.csv'
+train_data_path = '../data/IRIS_train.csv'
+test_data_path = '../data/IRIS_test.csv'
 
 train_data.to_csv(train_data_path, index=False)
 test_data.to_csv(test_data_path, index=False)
