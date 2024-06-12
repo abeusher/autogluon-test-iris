e541bf42f71790065576fa94e0d222fd9c27f6a0
Abe Usher <abe.usher@gmail.com>
2024-06-12 11:59:06 -0400

    update

--- iris_project/02_evaluate_model.py
+++ iris_project/02_evaluate_model.py
@@ -1,12 +1,20 @@
 from autogluon.tabular import TabularDataset, TabularPredictor
+import warnings
+warnings.filterwarnings("ignore", message="AutoGluon summary plots cannot be created because bokeh is not installed.")
 
 # load the model
+print("Loading model...")
 predictor = TabularPredictor.load('AutogluonModels/ag-20240612_155114')
+print("Done loading model.")
 
 # display the model's performance
 results = predictor.fit_summary()
 
 # show the model leaderboard
+print("Model Leaderboard:")
+print("************************************")
+print()
 test_data = TabularDataset('../data/iris_test.csv')
 leaderboard = predictor.leaderboard(test_data)
 print(leaderboard)
+print()
