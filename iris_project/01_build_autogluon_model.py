34df8ae60f9ea7a41db3799660daaed3bc50ec96
Abe Usher <abe.usher@gmail.com>
2024-06-12 12:07:14 -0400

    update

--- iris_project/01_build_autogluon_model.py
+++ iris_project/01_build_autogluon_model.py
@@ -9,11 +9,16 @@ test_data = TabularDataset('../data/iris_test.csv')
 # create a predictor object and fit it to the training data
 # see also: https://auto.gluon.ai/stable/tutorials/tabular/tabular-quick-start.html
 # see also: https://auto.gluon.ai/stable/tutorials/tabular/tabular-essentials.html
-predictor = TabularPredictor(label='species').fit(train_data=train_data)
+predictor = TabularPredictor(label='species').fit(train_data=train_data, presets="medium_quality")
+
+print("AutoGluon infers problem type is: ", predictor.problem_type)
+print("AutoGluon identified the following types of features:")
+print(predictor.feature_metadata)
+print()
 
 # make predictions on the test set
 predictions = predictor.predict(test_data)
 print("Predictions:")
 print("************************************")
-print()
-print(predictions)
+print("This prints the first 5 rows of the predictions:")
+print(predictions.head(5))
