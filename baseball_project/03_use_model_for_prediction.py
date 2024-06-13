ce9ef891532a85c22f4741d1461085a0606b2964
Abe <abe@blackcape.io>
2024-06-12 20:55:14 -0400

    update

--- baseball_project/03_use_model_for_prediction.py
+++ baseball_project/03_use_model_for_prediction.py
@@ -23,7 +23,8 @@ def main():
     # print a summary of how well it works
     # results = predictor.fit_summary()
 
-    test_data = TabularDataset('../data/raw_data_test.csv')
+    data_file = '../data/raw_data_test.csv'
+    test_data = TabularDataset(data_file)
     leaderboard = predictor.leaderboard(test_data)
     print(leaderboard)
     print()
@@ -43,18 +44,22 @@ def main():
     """
 
     # Define the column names as in the IRIS dataset
-    columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+    columns = ['Observation_ID', 'Y1=Pop 1', 'Y2=Pop 2', 'Y3=Pop 3', 'Y4=Pop 4', 'Y5=Pop 5', 'Y6= Pop 6', 
+           'X11', 'X12', 'X13', 'X14', 'X15', 'X21', 'X22', 'X23', 'X24', 'X25', 'X26', 'X31', 'X32', 
+           'X33', 'X34', 'X35', 'X36', 'X41', 'X42', 'X43', 'X44', 'X51', 'X52', 'X53', 'X54', 'X61', 
+           'X62', 'X63', 'X64', 'X65', 'X66', 'X67', 'X71', 'X72', 'X73', 'X74', 'X81', 'X82', 'X83', 
+           'X84', 'X85', 'X91', 'X92', 'X93', 'X94', 'X95', 'X96']
 
     # Define the row as a list and omit the species column that we are trying to predict
-    row = [7.9, 3.8, 6.4, 2.0]
+    row = [15396,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,0,x,0.373196537,5820,0.1,1.105170918,0.475020813,-0.74439666,0.475020813,-0.74439666,0.475020813,-0.74439666,0.475020813,-0.74439666,0.475020813,-0.74439666,0.475020813,-0.74439666]
 
     # Create a DataFrame
     row_df = pd.DataFrame([row], columns=columns)
 
     # predict the species of a single data point
-    correct_answer = 'Iris-virginica'
+    correct_answer = 0
     model_prediction= predictor.predict(row_df)
-    print("Evaluating the model on a single data point from data/iris_test.csv")
+    print("Evaluating the model on a single data point from %s" % data_file)
     print("input values: %s" %(row_df.values[0]))
     print(f"The model predicted: {model_prediction[0]}")
     print(f"The correct answer is: {correct_answer}")
