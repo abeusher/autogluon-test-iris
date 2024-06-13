d65bf3c6761f3831b54f3c520359fccadc5c9de4
Abe <abe@blackcape.io>
2024-06-12 20:06:47 -0400

    update

--- iris_project/02_evaluate_model.py
+++ iris_project/02_evaluate_model.py
@@ -1,20 +1,49 @@
+import os
 from autogluon.tabular import TabularDataset, TabularPredictor
 import warnings
 warnings.filterwarnings("ignore", message="AutoGluon summary plots cannot be created because bokeh is not installed.")
 
-# load the model
-print("Loading model...")
-predictor = TabularPredictor.load('AutogluonModels/ag-20240612_155114')
-print("Done loading model.")
+def list_subdirectories(parent_dir, prefix='ag-2024'):
+    """get a list of subdirectories in the parent directory that start with the prefix 'ag-2024' aka autogluon-2024"""
+    subdirectories = [d for d in os.listdir(parent_dir) if os.path.isdir(os.path.join(parent_dir, d)) and d.startswith(prefix)]
+    return subdirectories
 
-# display the model's performance
-results = predictor.fit_summary()
+def get_latest_subdirectory(parent_dir, prefix='ag-2024'):
+    """get the latest subdirectory in the parent directory that start with the prefix 'ag-2024' aka autogluon-2024"""
+    subdirectories = list_subdirectories(parent_dir, prefix)
+    print("subdirectories: ", subdirectories)
+    latest_subdirectory = max(subdirectories)
+    print("latest_subdirectory: ", latest_subdirectory)
+    return latest_subdirectory
 
-# show the model leaderboard
-print("Model Leaderboard:")
-print("************************************")
-print()
-test_data = TabularDataset('../data/iris_test.csv')
-leaderboard = predictor.leaderboard(test_data)
-print(leaderboard)
-print()
+def main():
+    # load the model
+    print("Loading model...")
+    # get the latest model directory
+    BASE_DIR = 'AutogluonModels'
+    model_dir = get_latest_subdirectory(BASE_DIR)
+    most_recent_model_dir = BASE_DIR+"/%s" %(model_dir)
+    print("Most recent model directory: %s" % most_recent_model_dir )
+    predictor = TabularPredictor.load(most_recent_model_dir)
+    print("Done loading model from (%s)." % most_recent_model_dir)
+
+    # display the model's performance
+    print("Model Performance:")
+    print("************************************")
+    print()
+    _ = predictor.fit_summary() # the underscore variable just means "ignore the output of this function call"
+    #print(results)
+    
+
+    # show the model leaderboard
+    print("Model Leaderboard:")
+    print("************************************")
+    print()
+    test_data = TabularDataset('../data/iris_test.csv')
+    leaderboard = predictor.leaderboard(test_data)
+    print(leaderboard)
+    print()
+
+
+if __name__ == '__main__':
+    main()
--- iris_project/04_load_specific_model_and_predict.py
+++ iris_project/04_load_specific_model_and_predict.py
@@ -1,36 +1,57 @@
 from autogluon.tabular import TabularDataset, TabularPredictor
 import pandas as pd
+import os
 
-
-# load the model
-#predictor = TabularPredictor.load('AutogluonModels/ag-20240612_132644/RandomForestEntr')
-predictor = TabularPredictor.load('AutogluonModels/ag-20240612_155114')
-model_names = predictor.model_names()
-print("Models available: ", model_names)
-print()
-
+#constants
 MODEL_TO_USE = "RandomForestGini"
 
 
-"""
-Lines 19-29 below test our model on a single data point.
-This data point is from the file data/iris_test.csv line #26
+def list_subdirectories(parent_dir, prefix='ag-2024'):
+    """get a list of subdirectories in the parent directory that start with the prefix 'ag-2024' aka autogluon-2024"""
+    subdirectories = [d for d in os.listdir(parent_dir) if os.path.isdir(os.path.join(parent_dir, d)) and d.startswith(prefix)]
+    return subdirectories
 
-Here's the row, note that the correct prediction for species is 'Iris-virginica':
-7.9,3.8,6.4,2.0,Iris-virginica
-"""
+def get_latest_subdirectory(parent_dir, prefix='ag-2024'):
+    """get the latest subdirectory in the parent directory that start with the prefix 'ag-2024' aka autogluon-2024"""
+    subdirectories = list_subdirectories(parent_dir, prefix)
+    print("subdirectories: ", subdirectories)
+    latest_subdirectory = max(subdirectories)
+    print("latest_subdirectory: ", latest_subdirectory)
+    return latest_subdirectory
 
-# Define the column names as in the IRIS dataset
-columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+def main():
+    # load the model
+    # previously the model was hard-coded
+    # predictor = TabularPredictor.load('AutogluonModels/ag-20240612_132644/RandomForestEntr')
+    # now we will instead load the latest model
+    model_path = get_latest_subdirectory('AutogluonModels')
+    # load the model
+    predictor = TabularPredictor.load("AutogluonModels/"+model_path)
+    model_names = predictor.model_names()
+    print("Models available: ", model_names)
+    print()
+    """
+    Lines 19-29 below test our model on a single data point.
+    This data point is from the file data/iris_test.csv line #26
 
-# Define the row as a list and omit the species column that we are trying to predict
-row = [7.9, 3.8, 6.4, 2.0]
+    Here's the row, note that the correct prediction for species is 'Iris-virginica':
+    7.9,3.8,6.4,2.0,Iris-virginica
+    """
 
-# Create a DataFrame
-row_df = pd.DataFrame([row], columns=columns)
+    # Define the column names as in the IRIS dataset
+    columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
 
-# predict the species of a single data point
-correct_answer = 'Iris-virginica'
-model_prediction= predictor.predict(row_df, model=MODEL_TO_USE)
-print(f"The model predicted: {model_prediction[0]}")
-print(f"The correct answer is: {correct_answer}")
+    # Define the row as a list and omit the species column that we are trying to predict
+    row = [7.9, 3.8, 6.4, 2.0]
+
+    # Create a DataFrame
+    row_df = pd.DataFrame([row], columns=columns)
+
+    # predict the species of a single data point
+    correct_answer = 'Iris-virginica'
+    model_prediction= predictor.predict(row_df, model=MODEL_TO_USE)
+    print(f"The model predicted: {model_prediction[0]}")
+    print(f"The correct answer is: {correct_answer}")
+
+if __name__ == '__main__':
+    main()
