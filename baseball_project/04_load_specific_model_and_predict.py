4a91aa062b446a0ca9d847a6658c499db6c8c622
Abe <abe@blackcape.io>
2024-06-12 21:02:51 -0400

    update

--- baseball_project/04_load_specific_model_and_predict.py
+++ baseball_project/04_load_specific_model_and_predict.py
@@ -1,10 +1,13 @@
-from autogluon.tabular import TabularDataset, TabularPredictor
+from autogluon.tabular import TabularPredictor
 import pandas as pd
 import os
 
 #constants
 MODEL_TO_USE = "RandomForestGini"
 
+from autogluon.tabular import TabularDataset, TabularPredictor
+import pandas as pd
+import os
 
 def list_subdirectories(parent_dir, prefix='ag-2024'):
     """get a list of subdirectories in the parent directory that start with the prefix 'ag-2024' aka autogluon-2024"""
@@ -20,38 +23,46 @@ def get_latest_subdirectory(parent_dir, prefix='ag-2024'):
     return latest_subdirectory
 
 def main():
-    # load the model
-    # previously the model was hard-coded
-    # predictor = TabularPredictor.load('AutogluonModels/ag-20240612_132644/RandomForestEntr')
-    # now we will instead load the latest model
     model_path = get_latest_subdirectory('AutogluonModels')
     # load the model
     predictor = TabularPredictor.load("AutogluonModels/"+model_path)
-    model_names = predictor.model_names()
-    print("Models available: ", model_names)
-    print()
-    """
-    Lines 19-29 below test our model on a single data point.
-    This data point is from the file data/iris_test.csv line #26
 
-    Here's the row, note that the correct prediction for species is 'Iris-virginica':
-    7.9,3.8,6.4,2.0,Iris-virginica
-    """
+    # print a summary of how well it works
+    # results = predictor.fit_summary()
+
+    data_file = '../data/raw_data_test.csv'
+    test_data = TabularDataset(data_file)
+    leaderboard = predictor.leaderboard(test_data)
+    print(leaderboard)
+    print()
+
+    # Outout the feature importance
+    feature_importance = predictor.feature_importance(test_data)
+    print(feature_importance)
+    print()
 
     # Define the column names as in the IRIS dataset
-    columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+    columns = ['Observation_ID', 'Y1=Pop 1',  'X11', 'X12', 'X13', 'X14', 'X15', 'X21', 'X22', 'X23', 'X24', 'X25', 'X26', 'X31', 'X32', 
+           'X33', 'X34', 'X35', 'X36', 'X41', 'X42', 'X43', 'X44', 'X51', 'X52', 'X53', 'X54', 'X61', 
+           'X62', 'X63', 'X64', 'X65', 'X66', 'X67', 'X71', 'X72', 'X73', 'X74', 'X81', 'X82', 'X83', 
+           'X84', 'X85', 'X91', 'X92', 'X93', 'X94', 'X95', 'X96']
 
     # Define the row as a list and omit the species column that we are trying to predict
-    row = [7.9, 3.8, 6.4, 2.0]
+    row = [15396,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,0]
 
     # Create a DataFrame
     row_df = pd.DataFrame([row], columns=columns)
 
     # predict the species of a single data point
-    correct_answer = 'Iris-virginica'
+    correct_answer = 0
+    ####
+    #### NOTE: we specify the specific model to use here!
     model_prediction= predictor.predict(row_df, model=MODEL_TO_USE)
+    print("Evaluating the model on a single data point from %s" % data_file)
+    print("input values: %s" %(row_df.values[0]))
     print(f"The model predicted: {model_prediction[0]}")
     print(f"The correct answer is: {correct_answer}")
+
 
 if __name__ == '__main__':
     main()
